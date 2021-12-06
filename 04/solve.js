4462623d01e75f33d931240439cf097fb1bb00a2
Conor Watson <conor.watson94@gmail.com>
2021-12-06 20:44:40 +0000

    feat: add solution for Day 04 Part 2

--- 04/solve.js
+++ 04/solve.js
@@ -17,10 +17,26 @@ const partOne = input => {
   return parseInt(sleepiestGuard.id) * sleepiestMinute;
 };
 
-const partTwo = input => {};
+const partTwo = input => {
+  const timeline = calculateTimeline(input);
+  const guards = calculateGuardShifts(timeline);
+
+  const mostFrequentlyAsleep = Object.values(guards).reduce(
+    (acc, curr) => {
+      const guardShiftTotals = findSleepiestMinute(curr);
+
+      if (guardShiftTotals.sleepCount > acc.sleepCount) return { ...guardShiftTotals, id: curr.id };
+
+      return acc;
+    },
+    { minute: 0, sleepCount: 0, id: null },
+  );
+
+  return parseInt(mostFrequentlyAsleep.id) * mostFrequentlyAsleep.minute;
+};
 
 const solve = (input, part) => (part === 1 ? partOne(input) : partTwo(input));
 
-const expected = part => (part === 1 ? 0 : 0);
+const expected = part => (part === 1 ? 38813 : 141071);
 
 module.exports = { solve, expected };
