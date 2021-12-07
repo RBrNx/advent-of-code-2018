2010adf39ccd7d0f6b8f60a6bf598204d49a5dc2
Conor Watson <conor.watson94@gmail.com>
2021-12-07 17:20:10 +0000

    feat: add solution for Day 05 Part 2

--- 05/solve.js
+++ 05/solve.js
@@ -1,5 +1,5 @@
-const partOne = input => {
-  let polymerArray = input[0].split('');
+const reactPolymer = polymer => {
+  let polymerArray = polymer.split('');
 
   for (let i = 0; i < polymerArray.length; i += 1) {
     const [currUnit, nextUnit] = [polymerArray[i], polymerArray[i + 1]];
@@ -12,13 +12,33 @@ const partOne = input => {
     }
   }
 
-  return polymerArray.length;
+  return polymerArray;
 };
 
-const partTwo = input => {};
+const partOne = input => reactPolymer(input[0]).length;
+
+const partTwo = input => {
+  const shortestPolymer = new Array(26).fill(null).reduce((acc, curr, i) => {
+    const unitsToRemove = [String.fromCodePoint(65 + i), String.fromCodePoint(97 + i)];
+    console.log(`Processing ${unitsToRemove}`);
+    const newPolymer = input[0]
+      .split('')
+      .filter(s => !unitsToRemove.includes(s))
+      .join('');
+    const condensedPolymer = reactPolymer(newPolymer);
+
+    console.log(`Length is ${condensedPolymer.length}, Current Length is ${acc?.length}`);
+
+    if (condensedPolymer.length < acc?.length) return condensedPolymer;
+
+    return acc;
+  }, input[0].split(''));
+
+  return shortestPolymer.length;
+};
 
 const solve = (input, part) => (part === 1 ? partOne(input) : partTwo(input));
 
-const expected = part => (part === 1 ? 'dabCBAcaDA' : 141071);
+const expected = part => (part === 1 ? 9390 : 5898);
 
 module.exports = { solve, expected };
