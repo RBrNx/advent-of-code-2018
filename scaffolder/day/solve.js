c8f3f0c02863c2c1f25b162520439144dbbeafc2
Conor Watson <conor.watson94@gmail.com>
2021-12-14 17:25:39 +0000

    feat: implement scaffolder script

---
+++ scaffolder/day/solve.js
@@ -0,0 +1,11 @@
+const {} = require('./helpers');
+
+const partOne = input => {};
+
+const partTwo = input => {};
+
+const solve = (input, part) => (part === 1 ? partOne(input) : partTwo(input));
+
+const expected = part => (part === 1 ? '' : '');
+
+module.exports = { solve, expected };
